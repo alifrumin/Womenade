97e8a9a4005eba44414407bb137f1c596e377eef
Alice Frumin <alifrumin@yahoo.com>
2015-12-10 01:20:17 -0500

    added some styling

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require 'sinatra/reloader'
 get "/" do
   erb :home
 end
+
+get "/about" do
+  erb :about
+end
+
+get "/links" do
+  erb :links
+end
+
+get "/other" do
+  erb :other
+end
+
+get "/start" do
+  erb :start
+end
